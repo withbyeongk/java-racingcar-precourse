69c0082bfdcdd6631b5a7b824934be5915d6036c
withbyeongk <withbyeongk@gmail.com>
2023-05-01 01:41:28 +0900

    Vehicle 인터페이스 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,24 @@
 package racingcar;
 
-public class Car {
+public class Car implements Vehicle {
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
     }
+
+	@Override
+	public String printWinner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int forwardCar() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
     // 추가 기능 구현
 }
---
+++ src/main/java/racingcar/Vehicle.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public interface Vehicle {
+	// 우승자 출력
+	public String printWinner();
+	// 자동차 전진 수
+	public int forwardCar();
+}
