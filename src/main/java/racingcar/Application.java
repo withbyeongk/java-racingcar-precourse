9efd611b8d2fe4535bd2a550c5c69c6aa93c4db1
withbyeongk <withbyeongk@gmail.com>
2023-05-07 06:07:41 +0900

    정상 동작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,4 @@
 package racingcar;
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
